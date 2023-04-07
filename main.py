4929a32dc6b2aec520fd76dd93a2dde78dca57c9
Brian T <125165526+JBBrian@users.noreply.github.com>
2023-04-06 22:08:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import smtplib
 import os
 import random
 
-SENDER = BOTEMAIL
+# CREATING CONSTANTS
+SENDER = BOT_EMAIL
 AUTH = os.environ.get("ETOKEN")
 TARGET = TARGET_EMAIL
 
+# CONVERTING DATA IN .TXT TO LIST OF LINES
 with open("insults.txt") as source:
     lines = source.readlines()
 
+# CREATING MESSAGE
 body = f"Dear Jeff,\n\n{random.choice(lines)}\nSincerely, \nYour Arch Nemesis"
 message = f"Subject: I hope this message finds you well\n\n{body}"
 
 print(f"--- SENDING.... ---\n\n{message}")
 
+# SENDING MESSAGE
 carrier = smtplib.SMTP("smtp.mail.yahoo.com", 587)
 carrier.starttls()
 carrier.login(SENDER, AUTH)
